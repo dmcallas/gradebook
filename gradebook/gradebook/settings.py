b8acc4995f92c7ecbd81297533f2437ae33ff59d
Daniel McAllaster <dmcallas@gmail.com>
2013-10-14 22:32:41 -0700

    Updating settings to enable database

--- gradebook/gradebook/settings.py
+++ gradebook/gradebook/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'gradebook',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'gradebook',
+        'PASSWORD': 'gradebook',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -83,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'pg#*y+n7@h@!!7knbq06_b6tl#o%lxct*4t+!do^+)hn@105ow'
+SECRET_KEY = 'pg#*y+n7@knbq06_b6t^+)hn@105owXoH7Uq1hHCwFG1ZIE7alW0IAMSL0ZPPl#o%lxctdoThwYfCIfTlR7nZJWsNcJlz'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
