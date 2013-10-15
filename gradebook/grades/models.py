f6d6f330807553ae4b9e61a0a1486278a1879dcb
Daniel McAllaster <dmcallas@gmail.com>
2013-10-15 10:05:48 -0700

    Fine-tuning models and adding __unicode__ methods.

--- gradebook/grades/models.py
+++ gradebook/grades/models.py
@@ -3,28 +3,40 @@ from django.db import models
 
 class EnrollmentStatus(models.Model):
 	description=models.TextField(unique=True,blank=False)
-	
+	def __unicode__(self):
+                return self.description
+
 class AssignmentType(models.Model):
 	description=models.TextField(unique=True,blank=False)
+	def __unicode__(self):
+                return self.description
 
 class Course(models.Model):
 	title=models.TextField(blank=False)
 	number=models.IntegerField(unique=True,blank=False)
+	def __unicode__(self):
+                return unicode(self.number) + ' - ' + self.title
 
 class Semester(models.Model):
 	name=models.TextField(blank=False)
 	year=models.IntegerField(blank=False)
+	def __unicode__(self):
+                return self.name + ' ' + unicode(self.year)
 
 class Section(models.Model):
 	course=models.ForeignKey(Course,blank=False,on_delete=models.PROTECT)
 	number=models.TextField(blank=False)
 	semester=models.ForeignKey(Semester,blank=False,on_delete=models.PROTECT)
+	def __unicode__(self):
+                return unicode(self.number) + ' (' + unicode(self.course) + ' - ' + unicode(self.semester) + ')'
 
 class Assignment(models.Model):
 	section=models.ForeignKey(Section,blank=False,on_delete=models.PROTECT)
 	name=models.TextField(blank=False)
 	max_value=models.FloatField(default=100)
 	assignment_type=models.ForeignKey(AssignmentType,blank=False,on_delete=models.PROTECT)
+	def __unicode__(self):
+                return self.name + ' (Section ' + self.section.number +')'
         class Meta:
                 unique_together=('section','name')
 
@@ -32,16 +44,21 @@ class Student(models.Model):
 	id=models.IntegerField(primary_key=True,unique=True,blank=False)
 	last_name=models.TextField(blank=False)
 	first_name=models.TextField(blank=False)
-	middle_name=models.TextField(null=True)
-	nick_name=models.TextField(null=True)
-	phone=models.TextField(null=True)
-	email=models.TextField(null=True)
-	suffix=models.TextField(null=True)
+	middle_name=models.TextField(null=True,blank=True)
+	nick_name=models.TextField(null=True,blank=True)
+	phone=models.TextField(null=True,blank=True)
+	email=models.TextField(null=True,blank=True)
+	suffix=models.TextField(null=True,blank=True)
+	def __unicode__(self):
+                return self.last_name + ', ' + self.first_name + ' (' + unicode(self.id) + ')'
+
 
 class AssignmentScore(models.Model):
 	student=models.ForeignKey(Student,blank=False,on_delete=models.PROTECT)
 	assignment=models.ForeignKey(Assignment,blank=False,on_delete=models.PROTECT)
-	score=models.FloatField(null=True)
+	score=models.FloatField(null=True,blank=True)
+	def __unicode__(self):
+                return 'Assignment: '+unicode(self.assignment) + ', Student: ' + unicode(self.student)
         class Meta:
                 unique_together=('student','assignment')
 
@@ -49,6 +66,8 @@ class Roster(models.Model):
 	section=models.ForeignKey(Section,blank=False,on_delete=models.PROTECT)
 	student=models.ForeignKey(Student,blank=False,on_delete=models.PROTECT)
 	enrollment_status=models.ForeignKey(EnrollmentStatus,blank=False,on_delete=models.PROTECT)
+	def __unicode__(self):
+                return unicode(self.section) + '/' + unicode(self.student)
         class Meta:
                 unique_together=('section','student')
 
@@ -56,9 +75,13 @@ class Attendance(models.Model):
 	student=models.ForeignKey(Student,blank=False,on_delete=models.PROTECT)
 	section=models.ForeignKey(Section,blank=False,on_delete=models.PROTECT)
 	date=models.DateField(blank=False)
+	def __unicode__(self):
+                return 'Section '+self.section.number+' - '+unicode(self.student) + ' - ' + unicode(self.date)
 
-class PermissionCodes(models.Model):
+class PermissionCode(models.Model):
 	permission_code=models.IntegerField(blank=False)
 	section=models.ForeignKey(Section,blank=False)
-	student=models.ForeignKey(Student,null=True)
+	student=models.ForeignKey(Student,null=True,blank=True)
+	def __unicode__(self):
+                return 'Section '+self.section.number + ': ' + unicode(self.permission_code)
 
