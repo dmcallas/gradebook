5a6892a376945a62dc865a6ea61a538e55fd2912
Daniel McAllaster <dmcallas@gmail.com>
2013-10-15 10:06:35 -0700

    Enabling all of the tables for grades in the admin panel

---
+++ gradebook/grades/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from grades.models import *
+
+admin.site.register(EnrollmentStatus)
+admin.site.register(AssignmentType)
+admin.site.register(Course)
+admin.site.register(Semester)
+admin.site.register(Section)
+admin.site.register(Assignment)
+admin.site.register(Student)
+admin.site.register(AssignmentScore)
+admin.site.register(Roster)
+admin.site.register(Attendance)
+admin.site.register(PermissionCode)
