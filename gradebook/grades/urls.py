5bbe67986ea9d8dfea308e34e7a13aef1c3fe84d
Daniel McAllaster <dmcallas@gmail.com>
2013-12-25 18:17:11 -0800

    Adding urls.py for the grades application

---
+++ gradebook/grades/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+from grades import views
+
+urlpatterns = patterns('',
+    # ex: /grades/
+    url(r'^$', views.section_index, name='section_index'),
+    # ex: /grades/5/
+    url(r'^(?P<section_id>\d+)/$', views.section, name='section'),
+    # ex: /grades/5/roster/
+    url(r'^(?P<section_id>\d+)/roster/$', views.roster, name='roster'),
+    # ex: /grades/5/assignmets/
+    url(r'^(?P<section_id>\d+)/assignments/$', views.assignments, name='assignments'),
+    # ex: /grades/5/students/1234/
+    url(r'^(?P<section_id>\d+)/roster/(?P<student_id>\d+)/$', views.roster_student, name='roster_student'),
+    # ex: /grades/5/hw_entry/
+    url(r'^(?P<section_id>\d+)/hw_entry/$', views.hw_entry, name='hw_entry'),
+
+)
