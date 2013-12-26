96a46b71657db220aa70dc43d5595e7c46c994ee
Daniel McAllaster <dmcallas@gmail.com>
2013-12-25 18:28:40 -0800

    Refining fields of model
    * Changing some of the text fields to char fields.
    * Adding letter grade fields.

--- gradebook/grades/models.py
+++ gradebook/grades/models.py
@@ -7,7 +7,7 @@ class Department(models.Model):
                 return self.long_name
         
 class EnrollmentStatus(models.Model):
-	description=models.TextField(unique=True,blank=False)
+	description=models.CharField(max_length=20,unique=True,blank=False)
 	def __unicode__(self):
                 return self.description
 
@@ -21,26 +21,32 @@ class Course(models.Model):
 	number=models.IntegerField(unique=True,blank=False)
         department=models.ForeignKey(Department,blank=False,on_delete=models.PROTECT)
 	def __unicode__(self):
-                return self.department.name+unicode(self.number) + ' - ' + self.title
+                return self.department.name+' '+unicode(self.number) + ' - ' + self.title
 
 class Semester(models.Model):
-	name=models.TextField(blank=False)
+	name=models.CharField(max_length=10,blank=False)
 	year=models.IntegerField(blank=False)
 	def __unicode__(self):
                 return self.name + ' ' + unicode(self.year)
 
 class Section(models.Model):
 	course=models.ForeignKey(Course,blank=False,on_delete=models.PROTECT)
-	number=models.TextField(blank=False)
+	number=models.CharField(max_length=10,blank=False)
 	semester=models.ForeignKey(Semester,blank=False,on_delete=models.PROTECT)
 	def __unicode__(self):
                 return unicode(self.number) + ' (' + unicode(self.course) + ' - ' + unicode(self.semester) + ')'
 
 class Assignment(models.Model):
 	section=models.ForeignKey(Section,blank=False,on_delete=models.PROTECT)
-	name=models.TextField(blank=False)
+	name=models.CharField(max_length=50,blank=False)
 	max_value=models.FloatField(default=100)
+        assign_date=models.DateField(null=True,blank=True)
+        due_date=models.DateField(null=True,blank=True)
 	assignment_type=models.ForeignKey(AssignmentType,blank=False,on_delete=models.PROTECT)
+        a_cutoff=models.FloatField(null=True,blank=True)
+        b_cutoff=models.FloatField(null=True,blank=True)
+        c_cutoff=models.FloatField(null=True,blank=True)
+        d_cutoff=models.FloatField(null=True,blank=True)
 	def __unicode__(self):
                 return self.name + ' (Section ' + self.section.number +')'
         class Meta:
@@ -48,21 +54,32 @@ class Assignment(models.Model):
 
 class Student(models.Model):
 	id=models.IntegerField(primary_key=True,unique=True,blank=False)
-	last_name=models.TextField(blank=False)
-	first_name=models.TextField(blank=False)
-	middle_name=models.TextField(null=True,blank=True)
-	nick_name=models.TextField(null=True,blank=True)
-	phone=models.TextField(null=True,blank=True)
-	email=models.TextField(null=True,blank=True)
-	suffix=models.TextField(null=True,blank=True)
+	last_name=models.CharField(max_length=50,blank=False)
+	first_name=models.CharField(max_length=50,blank=False)
+	middle_name=models.CharField(max_length=20,null=True,blank=True)
+	nick_name=models.CharField(max_length=50,null=True,blank=True)
+	phone=models.CharField(max_length=50,null=True,blank=True)
+	email=models.CharField(max_length=200,null=True,blank=True)
+	suffix=models.CharField(max_length=10,null=True,blank=True)
 	def __unicode__(self):
                 return self.last_name + ', ' + self.first_name + ' (' + unicode(self.id) + ')'
-
 
 class AssignmentScore(models.Model):
 	student=models.ForeignKey(Student,blank=False,on_delete=models.PROTECT)
 	assignment=models.ForeignKey(Assignment,blank=False,on_delete=models.PROTECT)
 	score=models.FloatField(null=True,blank=True)
+        def get_letter_grade(self):
+                if(self.assignment.a_cutoff):
+                        if(self.score>self.assignment.a_cutoff):
+                                return "A"
+                        if(self.score>self.assignment.b_cutoff):
+                                return "B"
+                        if(self.score>self.assignment.c_cutoff):
+                                return "C"
+                        if(self.score>self.assignment.d_cutoff):
+                                return "D"
+                        return "F"
+                return ""
 	def __unicode__(self):
                 return 'Assignment: '+unicode(self.assignment) + ', Student: ' + unicode(self.student)
         class Meta:
@@ -90,5 +107,3 @@ class PermissionCode(models.Model):
 	student=models.ForeignKey(Student,null=True,blank=True)
 	def __unicode__(self):
                 return 'Section '+self.section.number + ': ' + unicode(self.permission_code)
-
-        
