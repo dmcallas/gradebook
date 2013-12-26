e2fe042556012314b8bd796fb2db24028a251c45
Daniel McAllaster <dmcallas@gmail.com>
2013-12-25 18:21:06 -0800

    Adding admin docs URL, account login url, and foundation test url.

--- gradebook/gradebook/urls.py
+++ gradebook/gradebook/urls.py
@@ -10,8 +10,13 @@ urlpatterns = patterns('',
     # url(r'^gradebook/', include('gradebook.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^grades/', include('grades.urls', namespace="grades")),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'grades/login.html'}),
+    url(r'^foundation/',include('foundation.urls')),
 )
