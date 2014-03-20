2dc1b376405c31cc6f4ba74fbbf1ce4315590a6c
Daniel McAllaster <dmcallas@gmail.com>
2014-03-19 20:08:49 -0700

    Adding IDs to index for simplified Selenium testing

--- gradebook/functional_tests/tests.py
+++ gradebook/functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, LiveServerTestCase
+from grades import models
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -14,10 +15,23 @@ class SimpleTest(TestCase):
 
 class NewVisitorTest(LiveServerTestCase):        
     def setUp(self):
+        department=models.Department(name='MATH',long_name='Mathematics')
+        department.save()
+        course=models.Course(title='Math 100',number='100',department=department)
+        course.save()
+        semester=models.Semester(name='Fall',year=2014)
+        semester.save()
+        models.Section(course=course,number=1234,semester=semester).save()
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.close()
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url+'/grades/')
-        self.browser.save_screenshot('test.png')
+        self.browser.save_screenshot('test1.png')
+        self.browser.find_element_by_id('section-1-link').click()
+        self.browser.save_screenshot('test2.png')
+        # Check menus...
+        #self.browser.
         self.assertEqual(1+1,3)
+
+
